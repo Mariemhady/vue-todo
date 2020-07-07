b3eeda4f03227081e19ffbef45830b7096557c50
mariem <mariem_hady@outlook.com>
2020-07-08 01:10:32 +0200

    add store for crud operations

--- src/store.js
+++ src/store.js
@@ -4,13 +4,64 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
+    state: {
+        todos: [],
+        newTodo: ''
+    },
+    mutations: {
+        get(state, todo){
+            state.newTodo =  todo
+        },
+        add(state){
+            state.todos.push({
+                body: state.newTodo,
+                completed: false
+            })
+      		localStorage.setItem('todos', JSON.stringify(state.todos));
+        },
+        edit(state, todo){
+            var todos = state.todos
+            todos.splice(todos.indexOf(todo), 1)
+            state.todos = todos
+            state.newTodo = todo.body
+        },
+        remove(state, todo){
+            var todos = state.todos
+            todos.splice(todos.indexOf(todo), 1)
+            
+        },
+        complete(state, todo){
+            todo.completed = !todo.completed
+        },
+        clear(state){
+            state.newTodo = ''
+        }
+    },
+    actions: {
+        getTodo({commit}, todo){
+            commit('get', todo)
+        },
+        addTodo({commit}){
+            commit('add')
+        },
+        editTodo({commit}, todo){
+            commit('edit', todo)
+        },
+        removeTodo({commit}, todo){
+            commit('remove', todo)
+        },
+        completeTodo({commit}, todo){
+            commit('complete', todo)
+        },
+        clearTodo({commit}){
+            commit('clear')
+        }
 
-  },
-  mutations: {
+    },
+    getters: {
+        newTodo: state => state.newTodo,
+        todos: state => state.todos.filter((todo) => {return !todo.completed}),
+        completedTodos: state => state.todos.filter((todo) => {return todo.completed})
+    }
 
-  },
-  actions: {
-
-  }
 })
